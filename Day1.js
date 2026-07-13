b59867ee5e408df537dbe818930f9ad1742d2968
Ninjago618 <alvand597@gmail.com>
2026-07-13 22:01:30 +0300

    Create Day1.js

---
+++ Day1.js
@@ -0,0 +1,11 @@
+function fizzBuzz(n = 100) {
+  for (let i = 1; i <= n; i++) {
+    let out = "";
+    if (i % 3 === 0) out += "Fizz";
+    if (i % 5 === 0) out += "Buzz";
+    console.log(out || i);
+  }
+}
+
+// Run
+fizzBuzz();

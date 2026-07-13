fffc6256f2f0143378103e1883de5eb46f7e9e26
Ninjago618 <alvand597@gmail.com>
2026-07-13 22:02:53 +0300

    Create Day2.js

---
+++ Day2.js
@@ -0,0 +1,8 @@
+function reverseStringLoop(str) {
+  let res = "";
+  for (let i = str.length - 1; i >= 0; i--) res += str[i];
+  return res;
+}
+
+// Test
+console.log(reverseStringLoop("hello")); // "olleh"
